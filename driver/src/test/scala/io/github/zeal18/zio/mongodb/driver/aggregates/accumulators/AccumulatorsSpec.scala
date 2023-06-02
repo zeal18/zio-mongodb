911a56b049848e3080b0d4d45584992e9f04f52d
Aleksei Lezhoev <lezhoev@gmail.com>
2023-06-02 09:37:06 +0200

    Use expressions for accumulators

--- driver/src/test/scala/io/github/zeal18/zio/mongodb/driver/aggregates/accumulators/AccumulatorsSpec.scala
+++ driver/src/test/scala/io/github/zeal18/zio/mongodb/driver/aggregates/accumulators/AccumulatorsSpec.scala
@@ -1,6 +1,7 @@
 package io.github.zeal18.zio.mongodb.driver.aggregates.accumulators
 
 import io.github.zeal18.zio.mongodb.driver.aggregates.accumulators
+import io.github.zeal18.zio.mongodb.driver.aggregates.expressions
 import zio.test.ZIOSpecDefault
 import zio.test.*
 
@@ -19,36 +20,56 @@ object AccumulatorsSpec extends ZIOSpecDefault {
     }
 
   override def spec = suite("AccumulatorsSpec")(
-    testAccumulator("sum", accumulators.sum("a", 1), "a", """{"$sum": 1}"""),
-    testAccumulator("avg", accumulators.avg("a", 2), "a", """{"$avg": 2}"""),
-    testAccumulator("first", accumulators.first("a", 3), "a", """{"$first": 3}"""),
-    testAccumulator("last", accumulators.last("a", 4), "a", """{"$last": 4}"""),
-    testAccumulator("max", accumulators.max("a", 5), "a", """{"$max": 5}"""),
-    testAccumulator("min", accumulators.min("a", 6), "a", """{"$min": 6}"""),
-    testAccumulator("push", accumulators.push("a", 7), "a", """{"$push": 7}"""),
-    testAccumulator("addToSet", accumulators.addToSet("a", 8), "a", """{"$addToSet": 8}"""),
+    testAccumulator("sum", accumulators.sum("a", expressions.const(1)), "a", """{"$sum": 1}"""),
+    testAccumulator("avg", accumulators.avg("a", expressions.const(2)), "a", """{"$avg": 2}"""),
+    testAccumulator(
+      "first",
+      accumulators.first("a", expressions.const(3)),
+      "a",
+      """{"$first": 3}""",
+    ),
+    testAccumulator("last", accumulators.last("a", expressions.const(4)), "a", """{"$last": 4}"""),
+    testAccumulator("max", accumulators.max("a", expressions.const(5)), "a", """{"$max": 5}"""),
+    testAccumulator("min", accumulators.min("a", expressions.const(6)), "a", """{"$min": 6}"""),
+    testAccumulator("push", accumulators.push("a", expressions.const(7)), "a", """{"$push": 7}"""),
+    testAccumulator(
+      "addToSet",
+      accumulators.addToSet("a", expressions.const(8)),
+      "a",
+      """{"$addToSet": 8}""",
+    ),
     testAccumulator(
       "mergeObjects",
-      accumulators.mergeObjects("a", 9),
+      accumulators.mergeObjects("a", expressions.const(9)),
       "a",
       """{"$mergeObjects": 9}""",
     ),
-    testAccumulator("stdDevPop", accumulators.stdDevPop("a", 1), "a", """{"$stdDevPop": 1}"""),
-    testAccumulator("stdDevSamp", accumulators.stdDevSamp("a", 2), "a", """{"$stdDevSamp": 2}"""),
+    testAccumulator(
+      "stdDevPop",
+      accumulators.stdDevPop("a", expressions.const(1)),
+      "a",
+      """{"$stdDevPop": 1}""",
+    ),
+    testAccumulator(
+      "stdDevSamp",
+      accumulators.stdDevSamp("a", expressions.const(2)),
+      "a",
+      """{"$stdDevSamp": 2}""",
+    ),
     testAccumulator(
       "accumulator",
       accumulators.accumulator(
         fieldName = "a",
         initFunction = "initF",
-        initArgs = Seq("initArg"),
+        initArgs = Some(expressions.const(Seq("initArg"))),
         accumulateFunction = "accF",
-        accumulateArgs = Seq("accArg"),
+        accumulateArgs = Some(expressions.const(Seq("accArg"))),
         mergeFunction = "mergeF",
         finalizeFunction = Some("finalizeF"),
         lang = "lang",
       ),
       "a",
-      """{"$accumulator": {"init": "initF", "initArgs": ["initArg"], "accumulate": "accF", "accumulateArgs": ["accArg"], "merge": "mergeF", "lang": "lang", "finalize": "finalizeF"}}""",
+      """{"$accumulator": {"init": "initF", "initArgs": ["initArg"], "accumulate": "accF", "accumulateArgs": ["accArg"], "merge": "mergeF", "finalize": "finalizeF", "lang": "lang"}}""",
     ),
   )
 
