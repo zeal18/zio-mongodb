495972a4d6d7669cb68eab26505d79b6896ff7eb
Aleksei Lezhoev <lezhoev@gmail.com>
2023-06-12 20:16:09 +0200

    Add bucket aggregation support

--- driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
+++ driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
@@ -1,6 +1,7 @@
 package io.github.zeal18.zio.mongodb.driver
 
 import io.github.zeal18.zio.mongodb.bson.collection.immutable.Document
+import io.github.zeal18.zio.mongodb.driver.aggregates.accumulators
 import io.github.zeal18.zio.mongodb.driver.aggregates.expressions
 import io.github.zeal18.zio.mongodb.testkit.MongoClientTest
 import io.github.zeal18.zio.mongodb.testkit.MongoCollectionTest
@@ -108,6 +109,62 @@ object AggregationsItSpec extends ZIOSpecDefault {
             result1 <- collection.aggregate(aggregates.count()).runToChunk
             result2 <- collection.aggregate(aggregates.count("authors_count")).runToChunk
           } yield assertTrue(result1 == expected1 && result2 == expected2)
+        }
+      },
+      test("bucket") {
+        MongoCollectionTest.withRandomName[Document, TestResult] { collection =>
+          val documents = Chunk(
+            """{ "_id" : 1, "last_name" : "Bernard", "first_name" : "Emil", "year_born" : 1868, "year_died" : 1941, "nationality" : "France" }""",
+            """{ "_id" : 2, "last_name" : "Rippl-Ronai", "first_name" : "Joszef", "year_born" : 1861, "year_died" : 1927, "nationality" : "Hungary" }""",
+            """{ "_id" : 3, "last_name" : "Ostroumova", "first_name" : "Anna", "year_born" : 1871, "year_died" : 1955, "nationality" : "Russia" }""",
+            """{ "_id" : 4, "last_name" : "Van Gogh", "first_name" : "Vincent", "year_born" : 1853, "year_died" : 1890, "nationality" : "Holland" }""",
+            """{ "_id" : 5, "last_name" : "Maurer", "first_name" : "Alfred", "year_born" : 1868, "year_died" : 1932, "nationality" : "USA" }""",
+            """{ "_id" : 6, "last_name" : "Munch", "first_name" : "Edvard", "year_born" : 1863, "year_died" : 1944, "nationality" : "Norway" }""",
+            """{ "_id" : 7, "last_name" : "Redon", "first_name" : "Odilon", "year_born" : 1840, "year_died" : 1916, "nationality" : "France" }""",
+            """{ "_id" : 8, "last_name" : "Diriks", "first_name" : "Edvard", "year_born" : 1855, "year_died" : 1930, "nationality" : "Norway" }""",
+          ).map(d => Document(BsonDocument.parse(d)))
+
+          val expected =
+            Chunk(
+              Document(
+                BsonDocument.parse(
+                  """|{ "_id" : 1860, "count" : 4, "artists" : [
+                     |  { "name" : "Emil Bernard", "year_born" : 1868 },
+                     |  { "name" : "Joszef Rippl-Ronai", "year_born" : 1861 },
+                     |  { "name" : "Alfred Maurer", "year_born" : 1868 },
+                     |  { "name" : "Edvard Munch", "year_born" : 1863 }
+                     |]}""".stripMargin,
+                ),
+              ),
+            )
+
+          for {
+            _ <- collection.insertMany(documents)
+
+            result <- collection
+              .aggregate(
+                aggregates.bucket(
+                  groupBy = expressions.fieldPath("$year_born"),
+                  boundaries = Seq(1840, 1850, 1860, 1870, 1880),
+                  default = "Other",
+                  output = Map(
+                    "count" -> accumulators.sum(expressions.const(1)),
+                    "artists" -> accumulators.push(
+                      expressions.obj(
+                        "name" -> expressions.concat(
+                          expressions.fieldPath("$first_name"),
+                          expressions.const(" "),
+                          expressions.fieldPath("$last_name"),
+                        ),
+                        "year_born" -> expressions.fieldPath("$year_born"),
+                      ),
+                    ),
+                  ),
+                ),
+                aggregates.`match`(filters.gt("count", 3)),
+              )
+              .runToChunk
+          } yield assertTrue(result == expected)
         }
       },
     ).provideLayerShared(MongoClientTest.live().orDie)
