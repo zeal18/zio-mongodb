ae186a4d86a8c6a134b1916e2c3ed3dd0041c2d0
Aleksei Lezhoev <lezhoev@gmail.com>
2023-06-12 19:17:20 +0200

    Test count aggregation

--- driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
+++ driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
@@ -90,5 +90,25 @@ object AggregationsItSpec extends ZIOSpecDefault {
           } yield assertTrue(result1 == expected)
         }
       },
+      test("count") {
+        MongoCollectionTest.withRandomName[Document, TestResult] { collection =>
+          val documents = Chunk(
+            """{"_id": ObjectId("512bc95fe835e68f199c8686"), "author": "dave", "score": 80, "views": 100 }""",
+            """{"_id": ObjectId("55f5a1d3d4bede9ac365b25a"), "author": "li", "score": 94, "views": 999 }""",
+            """{"_id": ObjectId("55f5a192d4bede9ac365b257"), "author": "ahn", "score": 60, "views": 1000 }""",
+            """{"_id": ObjectId("55f5a192d4bede9ac365b258"), "author": "li", "score": 55, "views": 5000 }""",
+          ).map(d => Document(BsonDocument.parse(d)))
+
+          val expected1 = Chunk(Document(BsonDocument.parse("""{"count": 4}""")))
+          val expected2 = Chunk(Document(BsonDocument.parse("""{"authors_count": 4}""")))
+
+          for {
+            _ <- collection.insertMany(documents)
+
+            result1 <- collection.aggregate(aggregates.count()).runToChunk
+            result2 <- collection.aggregate(aggregates.count("authors_count")).runToChunk
+          } yield assertTrue(result1 == expected1 && result2 == expected2)
+        }
+      },
     ).provideLayerShared(MongoClientTest.live().orDie)
 }
