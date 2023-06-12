1add2d920b9f27c0cc87502d677e7a6b69f88f1f
Aleksei Lezhoev <lezhoev@gmail.com>
2023-06-12 19:11:27 +0200

    Test limit aggregation

--- driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
+++ driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
@@ -69,5 +69,26 @@ object AggregationsItSpec extends ZIOSpecDefault {
           }
         },
       ),
+      test("limit") {
+        MongoCollectionTest.withRandomName[Document, TestResult] { collection =>
+          val documents = Chunk(
+            """{"_id": ObjectId("512bc95fe835e68f199c8686"), "author": "dave", "score": 80, "views": 100 }""",
+            """{"_id": ObjectId("55f5a1d3d4bede9ac365b25a"), "author": "li", "score": 94, "views": 999 }""",
+            """{"_id": ObjectId("55f5a192d4bede9ac365b257"), "author": "ahn", "score": 60, "views": 1000 }""",
+            """{"_id": ObjectId("55f5a192d4bede9ac365b258"), "author": "li", "score": 55, "views": 5000 }""",
+          ).map(d => Document(BsonDocument.parse(d)))
+
+          val expected = Chunk(
+            """{"_id": ObjectId("512bc95fe835e68f199c8686"), "author": "dave", "score": 80, "views": 100 }""",
+            """{"_id": ObjectId("55f5a1d3d4bede9ac365b25a"), "author": "li", "score": 94, "views": 999 }""",
+          ).map(d => Document(BsonDocument.parse(d)))
+
+          for {
+            _ <- collection.insertMany(documents)
+
+            result1 <- collection.aggregate(aggregates.limit(2)).runToChunk
+          } yield assertTrue(result1 == expected)
+        }
+      },
     ).provideLayerShared(MongoClientTest.live().orDie)
 }
