1857d225fd0894d42cb4ff7d650c3074f2eaf102
Aleksei Lezhoev <lezhoev@gmail.com>
2023-06-13 18:53:55 +0200

    Get rid of unwind aggregation options

--- driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
+++ driver-it-tests/src/it/scala/io/github/zeal18/zio/mongodb/driver/AggregationsItSpec.scala
@@ -376,5 +376,105 @@ object AggregationsItSpec extends ZIOSpecDefault {
           } yield assertTrue(result == expected)
         }
       },
+      suite("unwind")(
+        test("array") {
+          MongoCollectionTest.withRandomName[Document, TestResult] { collection =>
+            val documents = Chunk(
+              """{ "_id" : 1, "item" : "ABC1", sizes: ["S", "M", "L"] }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            val expected = Chunk(
+              """{ "_id" : 1, "item" : "ABC1", "sizes" : "S" }""",
+              """{ "_id" : 1, "item" : "ABC1", "sizes" : "M" }""",
+              """{ "_id" : 1, "item" : "ABC1", "sizes" : "L" }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            for {
+              _ <- collection.insertMany(documents)
+
+              result <- collection.aggregate(aggregates.unwind("$sizes")).runToChunk
+            } yield assertTrue(result == expected)
+          }
+        },
+        test("missing and non-array values") {
+          MongoCollectionTest.withRandomName[Document, TestResult] { collection =>
+            val documents = Chunk(
+              """{ "_id" : 1, "item" : "Shirt", "sizes": [ "S", "M", "L"] }""",
+              """{ "_id" : 2, "item" : "Shorts", "sizes" : [ ] }""",
+              """{ "_id" : 3, "item" : "Hat", "sizes": "M" }""",
+              """{ "_id" : 4, "item" : "Gloves" }""",
+              """{ "_id" : 5, "item" : "Scarf", "sizes" : null }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            val expected = Chunk(
+              """{ _id: 1, item: 'Shirt', sizes: 'S' }""",
+              """{ _id: 1, item: 'Shirt', sizes: 'M' }""",
+              """{ _id: 1, item: 'Shirt', sizes: 'L' }""",
+              """{ _id: 3, item: 'Hat', sizes: 'M' }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            for {
+              _ <- collection.insertMany(documents)
+
+              result <- collection.aggregate(aggregates.unwind("$sizes")).runToChunk
+            } yield assertTrue(result == expected)
+          }
+        },
+        test("preserveNullAndEmptyArrays") {
+          MongoCollectionTest.withRandomName[Document, TestResult] { collection =>
+            val documents = Chunk(
+              """{ "_id" : 1, "item" : "ABC", price: NumberDecimal("80"), "sizes": [ "S", "M", "L"] }""",
+              """{ "_id" : 2, "item" : "EFG", price: NumberDecimal("120"), "sizes" : [ ] }""",
+              """{ "_id" : 3, "item" : "IJK", price: NumberDecimal("160"), "sizes": "M" }""",
+              """{ "_id" : 4, "item" : "LMN" , price: NumberDecimal("10") }""",
+              """{ "_id" : 5, "item" : "XYZ", price: NumberDecimal("5.75"), "sizes" : null }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            val expected = Chunk(
+              """{ "_id" : 1, "item" : "ABC", "price" : NumberDecimal("80"), "sizes" : "S" }""",
+              """{ "_id" : 1, "item" : "ABC", "price" : NumberDecimal("80"), "sizes" : "M" }""",
+              """{ "_id" : 1, "item" : "ABC", "price" : NumberDecimal("80"), "sizes" : "L" }""",
+              """{ "_id" : 2, "item" : "EFG", "price" : NumberDecimal("120") }""",
+              """{ "_id" : 3, "item" : "IJK", "price" : NumberDecimal("160"), "sizes" : "M" }""",
+              """{ "_id" : 4, "item" : "LMN", "price" : NumberDecimal("10") }""",
+              """{ "_id" : 5, "item" : "XYZ", "price" : NumberDecimal("5.75"), "sizes" : null }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            for {
+              _ <- collection.insertMany(documents)
+
+              result <- collection
+                .aggregate(aggregates.unwind("$sizes", preserveNullAndEmptyArrays = Some(true)))
+                .runToChunk
+            } yield assertTrue(result == expected)
+          }
+        },
+        test("includeArrayIndex") {
+          MongoCollectionTest.withRandomName[Document, TestResult] { collection =>
+            val documents = Chunk(
+              """{ "_id" : 1, "item" : "ABC", price: NumberDecimal("80"), "sizes": [ "S", "M", "L"] }""",
+              """{ "_id" : 2, "item" : "EFG", price: NumberDecimal("120"), "sizes" : [ ] }""",
+              """{ "_id" : 3, "item" : "IJK", price: NumberDecimal("160"), "sizes": "M" }""",
+              """{ "_id" : 4, "item" : "LMN" , price: NumberDecimal("10") }""",
+              """{ "_id" : 5, "item" : "XYZ", price: NumberDecimal("5.75"), "sizes" : null }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            val expected = Chunk(
+              """{ "_id" : 1, "item" : "ABC", "price" : NumberDecimal("80"), "sizes" : "S", "arrayIndex" : NumberLong(0) }""",
+              """{ "_id" : 1, "item" : "ABC", "price" : NumberDecimal("80"), "sizes" : "M", "arrayIndex" : NumberLong(1) }""",
+              """{ "_id" : 1, "item" : "ABC", "price" : NumberDecimal("80"), "sizes" : "L", "arrayIndex" : NumberLong(2) }""",
+              """{ "_id" : 3, "item" : "IJK", "price" : NumberDecimal("160"), "sizes" : "M", "arrayIndex" : null }""",
+            ).map(d => Document(BsonDocument.parse(d)))
+
+            for {
+              _ <- collection.insertMany(documents)
+
+              result <- collection
+                .aggregate(aggregates.unwind("$sizes", includeArrayIndex = Some("arrayIndex")))
+                .runToChunk
+            } yield assertTrue(result == expected)
+          }
+        },
+      ),
     ).provideLayerShared(MongoClientTest.live().orDie)
 }
